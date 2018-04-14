a9251a984b582cbfee3f40bd7f4bd582fe5e4dbe
gopy <gopystar@gmail.com>
2018-04-15 00:41:08 +0530

    Update Rout.js

--- client/src/Rout.js
+++ client/src/Rout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
